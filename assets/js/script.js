485d78ad3c864ed035fea59a1284337dbcb60160
mipear <>
2023-09-23 09:40:02 +0000

    Add basic html structure for heading and score area

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
 
-document.addEventListener('DOMContentLoaded', () => {
-    const cardArray = [
-        //need two of each card. perhaps not since matching definition//
+
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+// const cardArray = [
+    //need two of each card. perhaps not since matching definition//
+   // {
+      //  name: 'name',
+        //img: 'images/name.png'
+   // }
         {
-            name: 'name',
-            img: 'images/name.png'
-        }
-        {
-            name: 'name',
-            img: 'images/name.png'
-        }
-    ]
-})
+    //    name: 'name',
+   //     img: 'images/name.png'
+   // }
+//];
+ ///////////////////////
