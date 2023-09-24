c1d71fbc6467791e83c232ac96b2d1478468ebe7
mipear <>
2023-09-24 12:20:01 +0000

    Added card-area styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,46 +3,57 @@ document.addEventListener('DOMContentLoaded', () => {
    {
       name: 'National Speed Limit',
       img: 'images/nationalspeedlimit.webp'
-   }
+   },
    {
     name: 'No Stopping',
     img: 'image/nostopping.webp'
-   }
+   },
    {
     name: 'Mini Roundabout',
     img: 'images/miniroundabout.webp'
-   }
+   },
    {
     name: 'No Overtaking',
     img: 'images/noovertaking.webp'
-   }
+   },
    {
     name: 'Steep Downwards',
     img: 'images/steepdownwards.webp'
-   }
+   },
    {
    name: 'Frail Pedestrian Crossing',
     img: 'images/frailpedestrians.webp'
-   }
+   },
    {
     name: 'End of Motorway',
     img: 'images/endmotorway.webp'
-   }
+   },
    {
     name: 'With flow bus',
     img: 'assets/withflowbus.webp'
-   }
+   },
    {
     name: 'End Controlled',
     img: 'assets/endcontrolled.webp'
-   }
+   },
    {
     name: 'No Through Road',
     img: 'assets/nothroughroad.webp'
-   }
+   },
    {
     name: 'Camera',
     img: 'assets/camera.webp'
    }
-];
-}
+    ]
+
+const cardarea = document.querySelector('.card-area')
+
+
+
+
+
+
+
+
+
+})
