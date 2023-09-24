42f8773f7facac6762ddb8802f3bc827a5381d3d
mipear <>
2023-09-24 08:52:57 +0000

    Style game area and add sign images for cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,48 @@
-
-
-
-
-// document.addEventListener('DOMContentLoaded', () => {
-// const cardArray = [
-    //need two of each card. perhaps not since matching definition//
-   // {
-      //  name: 'name',
-        //img: 'images/name.png'
-   // }
-        {
-    //    name: 'name',
-   //     img: 'images/name.png'
-   // }
-//];
- ///////////////////////
+document.addEventListener('DOMContentLoaded', () => {
+    const cardArray = [
+   {
+      name: 'National Speed Limit',
+      img: 'images/nationalspeedlimit.webp'
+   }
+   {
+    name: 'No Stopping',
+    img: 'image/nostopping.webp'
+   }
+   {
+    name: 'Mini Roundabout',
+    img: 'images/miniroundabout.webp'
+   }
+   {
+    name: 'No Overtaking',
+    img: 'images/noovertaking.webp'
+   }
+   {
+    name: 'Steep Downwards',
+    img: 'images/steepdownwards.webp'
+   }
+   {
+   name: 'Frail Pedestrian Crossing',
+    img: 'images/frailpedestrians.webp'
+   }
+   {
+    name: 'End of Motorway',
+    img: 'images/endmotorway.webp'
+   }
+   {
+    name: 'With flow bus',
+    img: 'assets/withflowbus.webp'
+   }
+   {
+    name: 'End Controlled',
+    img: 'assets/endcontrolled.webp'
+   }
+   {
+    name: 'No Through Road',
+    img: 'assets/nothroughroad.webp'
+   }
+   {
+    name: 'Camera',
+    img: 'assets/camera.webp'
+   }
+];
+}
