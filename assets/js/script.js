6ecbff37cdb87aade5af2d68d6c6f38f07dcd826
mipear <>
2023-10-06 17:04:10 +0000

    resize image and change const gameArea

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,119 +1,75 @@
 // Wait for DOM to finish loading as suggested in Love Maths' 'Creating Event Listeners'//
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
     const cardArray = [
-   {
-      name: 'National Speed Limit',
-      img: 'images/nationalspeedlimit.webp'
-   },
-   {
-    name: 'No Stopping',
-    img: 'image/nostopping.webp'
-   },
-   {
-    name: 'Mini Roundabout',
-    img: 'images/miniroundabout.webp'
-   },
-   {
-    name: 'No Overtaking',
-    img: 'images/noovertaking.webp'
-   },
-   {
-    name: 'Steep Downwards',
-    img: 'images/steepdownwards.webp'
-   },
-   {
-   name: 'Frail Pedestrian Crossing',
-    img: 'images/frailpedestrians.webp'
-   },
-   {
-    name: 'End of Motorway',
-    img: 'images/endmotorway.webp'
-   },
-   {
-    name: 'With flow bus',
-    img: 'images/withflowbus.webp'
-   },
-   {
-    name: 'End Controlled',
-    img: 'images/endcontrolled.webp'
-   },
-   {
-    name: 'No Through Road',
-    img: 'images/nothroughroad.webp'
-   },
-   {
-    name: 'Camera',
-    img: 'images/camera.webp'
-   }
-    ]
-})
+        {
+            name: 'National Speed Limit',
+            img: 'asstes/images/nationalspeedlimit.webp'
+        },
+        {
+            name: 'No Stopping',
+            img: 'asstes/image/nostopping.webp'
+        },
+        {
+            name: 'Mini Roundabout',
+            img: 'asstes/images/miniroundabout.webp'
+        },
+        {
+            name: 'No Overtaking',
+            img: 'asstes/images/noovertaking.webp'
+        },
+        {
+            name: 'Steep Downwards',
+            img: 'asstes/images/steepdownwards.webp'
+        },
+        {
+            name: 'Frail Pedestrian Crossing',
+            img: 'asstes/images/frailpedestrians.webp'
+        },
+        {
+            name: 'End of Motorway',
+            img: 'asstes/images/endmotorway.webp'
+        },
+        {
+            name: 'With flow bus',
+            img: 'asstes/images/withflowbus.webp'
+        },
+        {
+            name: 'End Controlled',
+            img: 'asstes/images/endcontrolled.webp'
+        },
+        {
+            name: 'No Through Road',
+            img: 'assets/images/nothroughroad.webp'
+        },
+        {
+            name: 'Camera',
+            img: 'assets/images/camera.webp'
+        }
+    ];
 
- const gameArea = document.getElementsByClassName('.game-area');
+    cardArray.sort(() => 0.5 - Math.random());
+
+    const gameArea = document.querySelector('.game-area');
 
 
- //inspired by making 7 games video //
+    //inspired by making 7 games video //
 
- function runGame() {
-    for (let i = 0; i < cardArray.length; i++) {
-        var card = document.createElement('img')
-        card.setAttribute('src', 'images/camera.webp')
-        card.setAttribute('data-id', i)
-        card.addEventListener('click', flipCard)
-        gameArea.appendChild(card)
-    }
- }
-
-    let runGame = [];
-    let shuffleCards = [];
-    let flipCard = [];
-    let unflipCard = [];
-    let noClick = [];
-    let detectMatch = [];
-    let cardMatch = [];
-    let noMatch = [];
-    let incrementScore = [];
-
-    function shuffleCards() {
-        shuffleCards = [...cardArray];
-        for (let i = shuffleCards.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [shuffleCards[i],
-        shuffleCards[j]] = [shuffleCards[j], shuffleCards[i]];
+    function generateCards() {
+        for (let i = 0; i < cardArray.length; i++) {
+            var card = document.createElement('img');
+            card.setAttribute('src', 'assets/images/camera.webp');
+            card.setAttribute('card-index', i);
+            // card.addEventListener('click', flipCard);
+            gameArea.appendChild(card);
         }
     }
 
+    generateCards();
 
-    for (let gameArea of gameArea)(
-        gameArea.addEventListener("click"), function() {
-            if (this.getAttribute("datatype") === "flipCard") {
-            }
-        }
-    )
-
-    function cardClicked (event) {
-        if (event.target)
+    function flipCard() {
+        var cardIndex = this.getAttribute('card-index');
+        
     }
 
-function runGame(){
 
-}
-
-
-
-function flipCard() {
-// noClick() //
-}
-
-function detectMatch() {
-    if no matchMedia, unflipCard()
-    else keep flipCard, incrementScore()
-}
-
-function incrementScore() {
-
-}
-
-function resetGame() {
-
-}
-
+});
